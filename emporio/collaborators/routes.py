8956b936e22dc3f745546c97b876d7631889a24c
dtd_vsilva <dtd_vsilva@uolinc.com>
2023-05-29 16:17:13 -0300

    Adicionando opcao de deletar

--- emporio/collaborators/routes.py
+++ emporio/collaborators/routes.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Blueprint, request, render_template
 from emporio import Collaborator, db
 
@@ -9,3 +10,15 @@ def home():
     result = db.session.execute(
         db.select(Collaborator.collaborator_id, Collaborator.collaborator_name)).all()
     return render_template('collaborator/home.html', result=result)
+
+
+@collaborators.route("/collaborator", methods=['PUT'])
+def update_collaborator():
+    collaborator_form = request.get_json()
+    collaborator = db.session.execute(
+        db.select(Collaborator).where(Collaborator.collaborator_id == collaborator_form['collaborator_id'])
+    ).scalar_one()
+    print(collaborator.collaborator_name)
+    collaborator.collaborator_name = collaborator_form['collaborator_name']
+    db.session.commit()
+    return flask.Response(status=200)
--- emporio/static/js/init_modal_collaborator.js
+++ emporio/static/js/init_modal_collaborator.js
@@ -1,16 +1,22 @@
+import {set_loader} from "./loader.js";
+import {update_collaborator} from "./update_collaborator.js";
 export let sendCollaboratorUpdateForm
 
 export function init_modal_collaborator() {
-    $("#update").click(function (event) {
-        event.preventDefault();
+   $('.btn-floating.pink.accent-1').click(function () {
         $('#modal1').modal('open');
-        var td = $(this).parents('tr').find('td:lt(3)');
-
+        let td = $(this).parents('tr').find('td:lt(3)');
+        $('#name').val(td[0].textContent);
         $("#update-button").click(function (event) {
-            event.preventDefault();
-            console.log(td);
+            event.stopPropagation();
+            sendCollaboratorUpdateForm = {}
+            sendCollaboratorUpdateForm.collaborator_id = td[0].getAttribute('value')
+            sendCollaboratorUpdateForm.collaborator_name = $("#name").val()
+            set_loader();
+            setTimeout( function () {
+                update_collaborator()
+            }, 1500)
+            td[0].textContent = $("#name").val()
         })
-
-
     });
 }
